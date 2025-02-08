597e8a01034486af51df577de222f4c42fca1059
Vansh11223344 <guptavansh607@gmail.com>
2025-02-08 10:46:38 +0530

    FirstCommit

---
+++ script.js
@@ -0,0 +1,52 @@
+var currentSlide = 0;
+var slides = document.querySelectorAll('.slide');
+var totalSlides = slides.length;
+
+function showSlide(index) {
+    var slider = document.querySelector('.slides');
+    slider.style.transform = `translateX(-${index * 100}%)`; // Move slider based on index
+
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+// Move to the next slide
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+// Move to the previous slide
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+}
+
+// Auto-slide every 5 seconds
+var autoSlide = setInterval(nextSlide, 4000);
+
+function resetAutoSlide() {
+    clearInterval(autoSlide);
+    autoSlide = setInterval(nextSlide, 4000);
+}
+
+// Button event listeners
+document.querySelector('.next').addEventListener('click', () => {
+    nextSlide();
+    resetAutoSlide();
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    prevSlide();
+    resetAutoSlide();
+});
+
+// Initial slide setup
+showSlide(currentSlide);
+
+
+
